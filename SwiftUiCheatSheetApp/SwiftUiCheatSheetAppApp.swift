69923a88d230b843530d385e9cde4eb1643191c1
mrbergis <mrbergis@mail.ru>
2021-12-11 02:13:34 +0300

    Initial Commit

---
+++ SwiftUiCheatSheetApp/SwiftUiCheatSheetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCheatSheetAppApp.swift
+//  SwiftUiCheatSheetApp
+//
+//  Created by Андрей Адельбергис on 11.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCheatSheetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
